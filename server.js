f9a98c9a86883a180fcd8598f568b3c112170a97
SAAC7 <saulabraham881@gmail.com>
2026-03-07 21:05:45 -0600

    i prepare the page

--- server.js
+++ server.js
@@ -10,10 +10,21 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+const expressLayouts = require("express-ejs-layouts")
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
@@ -28,3 +39,4 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
