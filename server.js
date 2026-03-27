1802f997fc21767222cf6211a3b10034e54b3eb4
SAAC7_Hybrid <saulabraham881@gmail.com>
2026-03-27 16:44:58 -0600

    complete w3 Error Handling

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
+
 
 
 const expressLayouts = require("express-ejs-layouts")
@@ -27,7 +29,9 @@ app.use(static)
 //app.get("/", (req, res) => {
 //  res.render("index", { title: "Home" })
 //})
-app.get("/",baseController.buildHome)
+//app.get("/",baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
@@ -39,10 +43,33 @@ app.use("/inv", inventoryRoute)
 const port = process.env.PORT
 const host = process.env.HOST
 
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
 
